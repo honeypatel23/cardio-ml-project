9e7a4bd86a5874b98a85c900b64347d208db1b7a
honeypatel23 <honeypatel1423@gmail.com>
2026-03-08 16:23:33 +0530

    Added Streamlit app

---
+++ CardioProjectFrontend/streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+scaler = pickle.load(open("scaler.pkl","rb"))
+model = pickle.load(open("model.pkl","rb"))
+
+st.title("Cardiovascular Disease Prediction")
+
+age = st.number_input("Age (years)")
+gender = st.selectbox("Gender",[1,2])
+height = st.number_input("Height (cm)")
+weight = st.number_input("Weight (kg)")
+ap_hi = st.number_input("Systolic BP")
+ap_lo = st.number_input("Diastolic BP")
+cholesterol = st.selectbox("Cholesterol",[1,2,3])
+gluc = st.selectbox("Glucose",[1,2,3])
+smoke = st.selectbox("Smoke",[0,1])
+alco = st.selectbox("Alcohol",[0,1])
+active = st.selectbox("Physical Activity",[0,1])
+
+if st.button("Predict"):
+
+    age_days = age * 365.25
+
+    features = [[0, age_days, gender, height, weight, ap_hi, ap_lo,
+                 cholesterol, gluc, smoke, alco, active]]
+
+    scaled = scaler.transform(features)
+
+    prediction = model.predict(scaled)
+
+    if prediction[0] == 1:
+        st.error("High Risk of Cardiovascular Disease")
+    else:
+        st.success("Low Risk of Cardiovascular Disease")
